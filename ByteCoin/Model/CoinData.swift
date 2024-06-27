5356e299c05a37356078319168e77cabc904842e
Alexander <17aml08@gmail.com>
2024-06-27 16:22:15 +0300

    implement the project

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Alexander on 27.06.24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
